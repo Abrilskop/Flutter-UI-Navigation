1651a61f9297d7f4e5d844c3044223de3550445b
Abrilskop <uberpot.ia@gmail.com>
2025-02-20 15:36:32 -0500

    refactor UI: Add AppBar con título y personalizar colores del TabBar en TabsScreen

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -6,31 +6,28 @@ class TabsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,  // Define el número de pestañas
+      length: 4, // Número de pestañas
       child: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: const [
-              TabBar(
-                labelColor: Colors.black,  // Color de la etiqueta seleccionada
-                unselectedLabelColor: Colors.grey,  // Color de la etiqueta no seleccionada
-                tabs: [
-                  Tab(text: 'Tab 1'),  // Pestaña 1
-                  Tab(text: 'Tab 2'),  // Pestaña 2
-                  Tab(text: 'Tab 3'),  // Pestaña 3
-                  Tab(text: 'Tab 4'),  // Pestaña 4
-                ],
-              ),
-              Expanded(
-                child: TabBarView(
-                  children: [
-                    Icon(Icons.directions_car_outlined, size: 100),  // Ícono para la primera pestaña
-                    Icon(Icons.directions_transit_filled_outlined, size: 100),  // Ícono para la segunda pestaña
-                    Icon(Icons.directions_bike_sharp, size: 100),  // Ícono para la tercera pestaña
-                    Icon(Icons.directions_walk_sharp, size: 100),  // Ícono para la cuarta pestaña
-                  ],
-                ),
-              ),
+        appBar: AppBar(
+          title: const Text('TAXI CONO!'),
+          bottom: const TabBar(
+            labelColor: Color.fromARGB(255, 194, 66, 66),
+            unselectedLabelColor: Colors.grey,
+            tabs: [
+              Tab(icon: Icon(Icons.directions_car_outlined)), // Ícono para la primera pestaña
+              Tab(icon: Icon(Icons.directions_transit_filled_outlined)), // Ícono para la segunda pestaña
+              Tab(icon: Icon(Icons.directions_bike_sharp)), // Ícono para la tercera pestaña
+              Tab(icon: Icon(Icons.directions_walk_sharp)), // Ícono para la cuarta pestaña
+            ],
+          ),
+        ),
+        body: const SafeArea(
+          child: TabBarView(
+            children: [
+              Icon(Icons.directions_car_outlined, size: 100),
+              Icon(Icons.directions_transit_filled_outlined, size: 100),
+              Icon(Icons.directions_bike_sharp, size: 100),
+              Icon(Icons.directions_walk_sharp, size: 100),
             ],
           ),
         ),
